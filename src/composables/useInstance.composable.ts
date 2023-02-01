47352ca392502d6a3ea74ce9d3578faa33de76c8
stavros.mavromatis <stavros.mavromatis@arx.net>
2023-02-01 16:48:57 +0200

    finally done

--- src/composables/useInstance.composable.ts
+++ src/composables/useInstance.composable.ts
@@ -30,3 +30,10 @@ export function useAxiosInstance() {
 // VITE_CLIENT_SECRET=6bzk9trfo4y8rx32qmkkmai10hkte9agnks9qg0phr9wgsoe9g
 //VITE_REDIRECT_URI=http://127.0.0.1:5132/auth
 //VITE_BACK_END_URI = http://localhost:5301/api
+
+//NEW CREDENTIALS DHLWSEIS 6 APP\
+// VITE_BASE_URI=http://localhost:5132/
+// VITE_REDIRECT_URI=http://localhost:5132/auth
+// VITE_BACK_END_URI = http://localhost:5301/api
+// VITE_CLIENT_SECRET=0o603mdj68ppdziwfcmhbh73v8rr7tfs3874q57no64h4yra89
+// VITE_CLIENT_ID=63da7a8d7cf7bb02d67317e5
