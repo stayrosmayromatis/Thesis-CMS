b0321965d9bd87b95f41e192a28556d8881710ec
stavros.mavromatis <stavros.mavromatis@arx.net>
2023-02-15 17:13:49 +0200

    wip

--- src/enums/LabSemesterEnum.ts
+++ src/enums/LabSemesterEnum.ts
@@ -1,9 +1,14 @@
 export enum LabSemesterEnum{
-    A_XEIM=0,
-    B_EAR=1,
-    C_XEIM=2,
-    D_EAR=3,
-    E_XEIM=4,
-    ST_EAR=5,
-    Z_XEIM=6
+    A_XEIM=1,
+    B_EAR=2,
+    C_XEIM=3,
+    D_EAR=4,
+    E_XEIM=5,
+    ST_EAR=6,
+    Z_XEIM=7,
+    H_EAR=8,
+    TH_XEIM=9,
+    I_EAR=10,
+    K_XEIM=11,
+    L_EAR=12
   }
